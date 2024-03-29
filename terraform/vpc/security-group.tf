cae3611a4580a45c0e8f5c1deae11e00d869c1fa
loki <loki_snow@163.com>
2024-03-29 16:55:43 +0800

    add 8086 ingress

--- terraform/vpc/security-group.tf
+++ terraform/vpc/security-group.tf
@@ -34,6 +34,14 @@ resource "aws_security_group" "sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    description = "http"
+    from_port   = 8086
+    to_port     = 8086
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
